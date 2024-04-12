4bbd80a27e166b978299e498c3de93c63e2a63e3
Zenxah <p2662029@my365.dmu.ac.uk>
2024-04-12 12:48:25 +0100

    Fixed Issues

---
+++ Assets/USE_INPUT_SYSTEM_POSE_CONTROL.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class USE_INPUT_SYSTEM_POSE_CONTROL : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
